d16d7b13dca99dfc0bef4b6d793c98ab188c4b9d
safayet-hossian <shehab.shafayet@gmail.com>
2024-12-07 19:25:04 +0600

    initialize vue in laravel project

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
+<template>
+    <div>
+        <h2>Welcome to Laravel & VueJS app!</h2>
+        <div>
+            <div>{{ count }}</div>
+            <button type="button" @click="count++">Add</button>
+            <button type="button" @click="count--">Minus</button>
+        </div>
+    </div>
+</template>
